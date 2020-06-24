e9867e385323fda58f4238b9a114439cf23a9d42
fmazur57u <66006365+fmazur57u@users.noreply.github.com>
2020-06-24 14:01:55 +0200

    Add files via upload

--- correct_DNA.py
+++ correct_DNA.py
@@ -9,16 +9,17 @@ import numpy as np
 from MDPlus.core import *
 from scipy.interpolate import interp2d
 import mdtraj as md
-
-
-pdb = 'Florian_dna/182D_noInterc.pdb'
+import sys
 
 def correct_dna(pdb):
 
     a = open(pdb, 'r')
     f_list = []
-    information_list= []
+    information_residue= []
+    information_strand = []
     information_list_pdb = []
+    i_residue = []
+    i_strand = []
     pdb_list = []     
     pdb_start       = []
     pdb_last        = []
@@ -38,54 +39,69 @@ def correct_dna(pdb):
         if as_List[0] == "ATOM":
             #We don't use hydrogen atom for use RX structure or NMR structure or other
             if as_List[2] in use_atoms:
+                if as_List[4] in ['A', 'B']: 
+                    information_strand.append(as_List[4])
+                
+                    information_residue.append(int(as_List[len(as_List)-7]))
 
-                information_list.append([float(as_List[len(as_List)-7]), as_List[len(as_List)-9],\
-                                     as_List[len(as_List)-10], float(as_List[len(as_List)-6]),\
-                               float(as_List[len(as_List)-5]), float(as_List[len(as_List)-4])])
+                    information_list_pdb.append(line)
+          
+    for i in range(len(information_strand)):
+        if information_strand[i] != information_strand[i - 1]:
+            i_strand.append(information_strand[i])
+    
+    #If the strucutre is just one strand, there is zero dna helix
+    if len(i_strand) < 2:
+        print('zero dna helix')
+        sys.exit()
+        
+    for i in range(len(information_residue)):
+        if information_residue[i] != information_residue[i - 1]:
+            i_residue.append(information_residue[i])
 
-                information_list_pdb.append(line)
-
-    last_information = information_list[-1]
-    max_index = last_information[0]
-
-
-    ##not clear what is happening here
     #Loop for create a dyads_list for crd format and for pdb format
-    for p in range(int(2), int((max_index/2) + 2), int(2)):
+    for p in range(int(0), int(len(i_residue)/2), int(2)):
         residue_list_pdb = []
         for i in information_list_pdb:
             as_Liste = i.split(" ")
             as_List = [elem for elem in as_Liste if elem.strip()]
-            if float(as_List[5]) == p -1 or \
-               float(as_List[5]) == p or \
-               float(as_List[5]) == (max_index + 1) - p or \
-               float(as_List[5]) == (max_index + 1) - (p - 1):
-                    residue_list_pdb.append(i)
+            if float(as_List[5]) == i_residue[p] or \
+               float(as_List[5]) == i_residue[p + 1]:
+                   if as_List[4] == i_strand[0]:
+                       residue_list_pdb.append(i)
+                       
+            if float(as_List[5]) == i_residue[-p - 2] or \
+               float(as_List[5]) == i_residue[-p - 1]:
+                   if as_List[4] == i_strand[1]: 
+                       residue_list_pdb.append(i)
         pdb_list.append(residue_list_pdb)
     np.savetxt('pdb_start.pdb', pdb_list[0], fmt = '%s')
     
     start = md.load('pdb_start.pdb')
     start.save('pdb_start.pdb')
-    #Delete some other atom in backbone for obtain a dyads with same number of atom
+    
+    #Delete some other atom in backbone for obtain a dyads with same number of atom. For the first dyads.
     o = open('pdb_start.pdb', 'r')
     for line in o:
         as_Liste = line.split(" ")
         as_List = [elem for elem in as_Liste if elem.strip()]
         if as_List[0] == "ATOM":
-            if float(as_List[5]) != 1 and \
-               float(as_List[5]) != max_index - 1:
+            if float(as_List[5]) != i_residue[0] and \
+               float(as_List[5]) != i_residue[-2]:
                    if as_List[2] not in dont_use_atom_2:
                        pdb_start.append(line)
                    
-            if float(as_List[5]) == 1 or float(as_List[5]) == max_index - 1:
+            if float(as_List[5]) == i_residue[0] or float(as_List[5]) == i_residue[-2]:
                    if as_List[2] not in dont_use_atom_1:
                        pdb_start.append(line)
+                       
+    #Sometimes with some dna structure files, some important atom are missing. 
     if len(pdb_start) == 52:
         np.savetxt('dyads_dna[0].pdb', pdb_start, fmt='%s')
         f0 = Fasu('dyads_dna[0].pdb')
         f_list.append(f0)
     
-    
+    #Delete some other atom in backbone for obtain a dyads with same number of atom. For all middle dyads.
     for i_dyads_middle in range(1, len(pdb_list) - 1):
         np.savetxt('pdb_middle.pdb', pdb_list[i_dyads_middle], fmt='%s')
         middle = md.load('pdb_middle.pdb')
@@ -96,14 +112,30 @@ def correct_dna(pdb):
             as_Liste = line.split(" ")
             as_List = [elem for elem in as_Liste if elem.strip()]
             if as_List[0] == "ATOM":
-                if int(as_List[5])%2 ==0:
-                    if as_List[2] not in dont_use_atom_2:
-                        pdb_middle.append(line)
-                else:
-                    if as_List[2] not in dont_use_atom_1:
-                        pdb_middle.append(line)
-        if len(pdb_middle) == 52: 
-            np.savetxt('dyads_dna_%f.pdb' % i_dyads_middle, pdb_middle, fmt='%s')
+                for i in range(len(i_residue)):
+                        if i%2 ==0:
+                            if int(as_List[5]) == i_residue[i]:
+                                if as_List[2] not in dont_use_atom_1:
+                                    pdb_middle.append(line)
+                                
+                        else:
+                            if int(as_List[5]) == i_residue[i]:
+                                if as_List[2] not in dont_use_atom_2:
+                                    pdb_middle.append(line)
+        """                            
+        Sometimes with some files this programm take two time each line.
+        This loop correct this error.
+        If this error doesn't exist, this loop doesn't change the result.
+        """
+        correct_pdb_middle = []
+        for i in range(len(pdb_middle)):
+            if pdb_middle[i] != pdb_middle[i - 1]:
+                correct_pdb_middle.append(pdb_middle[i])
+                
+                
+        #Sometimes with some dna structure files, some important atom are missing.    
+        if len(correct_pdb_middle) == 52:
+            np.savetxt('dyads_dna_%f.pdb' % i_dyads_middle, correct_pdb_middle, fmt='%s')
             f_m = Fasu('dyads_dna_%f.pdb' % i_dyads_middle)
             f_list.append(f_m)
     
@@ -111,21 +143,23 @@ def correct_dna(pdb):
     
     last = md.load('pdb_last.pdb')
     last.save('pdb_last.pdb')
-    #Delete some other atom in backbone for obtain a dyads with same number of atom
+    
+    #Delete some other atom in backbone for obtain a dyads with same number of atom. For the last dyads.
     o = open('pdb_last.pdb', 'r')
     for line in o:
         as_Liste = line.split(" ")
         as_List = [elem for elem in as_Liste if elem.strip()]
         if as_List[0] == "ATOM":
-            if int(as_List[5]) != int(max_index // 2) - 1 and \
-               int(as_List[5]) != int(max_index // 2) + 1:
+            if int(as_List[5]) != i_residue[int(len(i_residue) / 2) - 2] and \
+               int(as_List[5]) != i_residue[int(len(i_residue) / 2)]:
                    if as_List[2] not in dont_use_atom_2:
                        pdb_last.append(line)
                    
-            if int(as_List[5]) == int(max_index // 2) - 1 or int(as_List[5]) == int(max_index // 2) + 1:
+            if int(as_List[5]) == i_residue[int(len(i_residue) / 2) - 2] or int(as_List[5]) == i_residue[int(len(i_residue) / 2)]:
                    if as_List[2] not in dont_use_atom_1:
                        pdb_last.append(line)
-    
+                       
+    #Sometimes with some dna structure files, some important atom are missing.
     if len(pdb_last) == 52:
         np.savetxt('dyads_dna[-1].pdb', pdb_last, fmt='%s')
         f_last = Fasu('dyads_dna[-1].pdb')
@@ -219,10 +253,12 @@ def correct_dna(pdb):
     #Correction[0] = energy correction and Correction[1] = force correction
     Correction = [Correction_E_dna, Correction_F_dna]
     return Correction
+  
+if __name__ == "__main__":
+    
+    pdb = 'Florian_dna/182D_noInterc.pdb'
+    print(correct_dna(pdb))
+    
 
-##test code, does not run when this code is imported as a module, only when 
-##called as an appli   
-if __name__ == "__main__":  
-     print(correct_dna(pdb))
-       
+      
     
